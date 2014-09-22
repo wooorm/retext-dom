04f2a2b40309ddc2480e0d9b978ae4848930f465
wooorm <tituswormer@gmail.com>
2014-09-22 13:42:49 +0200

    Refactor spec
    Includes a rewrite for retext ^0.2.0-rc.1

--- spec/retext-dom.spec.js
+++ spec/retext-dom.spec.js
@@ -1,6 +1,15 @@
 'use strict';
 
-var retextDOM, jsdom, Retext, assert, TextOM, retext;
+var dom,
+    jsdom,
+    Retext,
+    assert,
+    TextOM,
+    retext;
+
+/**
+ * JSDOM dependency.
+ */
 
 /* istanbul ignore else */
 if (typeof window !== 'object') {
@@ -10,250 +19,357 @@ if (typeof window !== 'object') {
     global.document = window.document;
 }
 
-retextDOM = require('..');
+/**
+ * Module dependencies.
+ */
 
+dom = require('..');
 Retext = require('retext');
 assert = require('assert');
 
-retext = new Retext().use(retextDOM);
-TextOM = retext.parser.TextOM;
+retext = new Retext().use(dom);
+TextOM = retext.TextOM;
+
+/**
+ * Unit tests for `retext-dom`.
+ */
 
 describe('retext-dom', function () {
-    it('should be of type `function`', function () {
-        assert(typeof retextDOM === 'function');
+    it('should be a `function`', function () {
+        assert(typeof dom === 'function');
     });
 
-    it('should export an `attach` method', function () {
-        assert(typeof retextDOM.attach === 'function');
-    });
-
-    it('should create a DOM tree from the TextOM tree', function () {
-        var tree = retext.parse('Some English words.'),
-            $div = tree.toDOMNode(),
-            sentenceNode = tree.head.head,
-            $sentence = $div.childNodes[0].childNodes[0];
-
-        /* Validate Paragraph node. */
-        assert($div.childNodes[0] === tree.head.toDOMNode());
-
-        /* Validate Sentence node. */
-        assert($sentence === sentenceNode.toDOMNode());
-
-        /* validate Text nodes */
-        assert($sentence.childNodes[0] === sentenceNode[0].toDOMNode());
-        assert($sentence.childNodes[1] === sentenceNode[1].toDOMNode());
-        assert($sentence.childNodes[2] === sentenceNode[2].toDOMNode());
-        assert($sentence.childNodes[3] === sentenceNode[3].toDOMNode());
-        assert($sentence.childNodes[4] === sentenceNode[4].toDOMNode());
-        assert($sentence.childNodes[5] === sentenceNode[5].toDOMNode());
-        assert($sentence.childNodes.length === sentenceNode.length);
-
-        /* validate HTML */
-        assert($div.outerHTML === '<div>' +
-                '<p>' +
-                    '<span>' +
-                        '<span>' +
-                            'Some' +
-                        '</span>' +
-                        '<span>' +
-                            ' ' +
-                        '</span>' +
-                        '<span>' +
-                            'English' +
-                        '</span>' +
-                        '<span>' +
-                            ' ' +
-                        '</span>' +
-                        '<span>' +
-                            'words' +
-                        '</span>' +
-                        '<span>' +
-                            '.' +
-                        '</span>' +
-                    '</span>' +
-                '</p>' +
-            '</div>'
-        );
-    });
-
-    it('should update the DOM when nodes are removed/inserted', function () {
-        var tree = retext.parse('Some English words.'),
-            $div = tree.toDOMNode(),
-            sentenceNode = tree.head.head,
-            wordNode = sentenceNode[2],
-            whiteSpaceNode = sentenceNode[3],
-            $sentence = $div.childNodes[0].childNodes[0];
-
-        /* Remove “English” and it following space */
-        wordNode.remove();
-        whiteSpaceNode.remove();
-
-        /* validate Text nodes */
-        assert($sentence.childNodes[0] === sentenceNode[0].toDOMNode());
-        assert($sentence.childNodes[1] === sentenceNode[1].toDOMNode());
-        assert($sentence.childNodes[2] === sentenceNode[2].toDOMNode());
-        assert($sentence.childNodes[3] === sentenceNode[3].toDOMNode());
-        assert($sentence.childNodes.length === sentenceNode.length);
-
-        /* validate HTML */
-        assert(
-            $div.outerHTML === '<div>' +
-                '<p>' +
-                    '<span>' +
-                        '<span>' +
-                            'Some' +
-                        '</span>' +
-                        '<span>' +
-                            ' ' +
-                        '</span>' +
-                        '<span>' +
-                            'words' +
-                        '</span>' +
-                        '<span>' +
-                            '.' +
-                        '</span>' +
-                    '</span>' +
-                '</p>' +
-            '</div>'
-        );
-
-        /* Insert “English” and it following space */
-        sentenceNode[1].after(whiteSpaceNode);
-        whiteSpaceNode.before(wordNode);
-
-        /* validate Text nodes */
-        assert($sentence.childNodes[0] === sentenceNode[0].toDOMNode());
-        assert($sentence.childNodes[1] === sentenceNode[1].toDOMNode());
-        assert($sentence.childNodes[2] === sentenceNode[2].toDOMNode());
-        assert($sentence.childNodes[3] === sentenceNode[3].toDOMNode());
-        assert($sentence.childNodes[4] === sentenceNode[4].toDOMNode());
-        assert($sentence.childNodes[5] === sentenceNode[5].toDOMNode());
-        assert($sentence.childNodes.length === sentenceNode.length);
-
-        /* validate HTML */
-        assert($div.outerHTML === '<div>' +
-                '<p>' +
-                    '<span>' +
-                        '<span>' +
-                            'Some' +
-                        '</span>' +
-                        '<span>' +
-                            ' ' +
-                        '</span>' +
-                        '<span>' +
-                            'English' +
-                        '</span>' +
-                        '<span>' +
-                            ' ' +
-                        '</span>' +
-                        '<span>' +
-                            'words' +
-                        '</span>' +
-                        '<span>' +
-                            '.' +
-                        '</span>' +
-                    '</span>' +
-                '</p>' +
-            '</div>'
-        );
-    });
-
-    it('should update the DOM when a text value changes', function () {
-        var tree = retext.parse('Some English words.'),
-            $div = tree.toDOMNode();
-
-        /* Change the terminal marker from a full-stop to a bang */
-        tree.head.head.tail.head.fromString('!');
-
-        /* Validate text content */
-        assert(tree.head.head.tail.toDOMNode().textContent === '!');
-
-        /* validate HTML */
-        assert($div.outerHTML === '<div>' +
-                '<p>' +
-                    '<span>' +
-                        '<span>' +
-                            'Some' +
-                        '</span>' +
-                        '<span>' +
-                            ' ' +
-                        '</span>' +
-                        '<span>' +
-                            'English' +
-                        '</span>' +
-                        '<span>' +
-                            ' ' +
-                        '</span>' +
-                        '<span>' +
-                            'words' +
-                        '</span>' +
-                        '<span>' +
-                            '!' +
-                        '</span>' +
-                    '</span>' +
-                '</p>' +
-            '</div>'
-        );
+    it('should have an `attach` method', function () {
+        assert(typeof dom.attach === 'function');
     });
 });
 
-describe('TextOM.Node#', function () {
-    it('should have a `toDOMNode` method', function () {
-        assert(typeof (new TextOM.Node()).toDOMNode === 'function');
-    });
+/**
+ * Unit tests for added properties on `TextOM.Node#`.
+ */
 
+describe('Node#', function () {
     it('should have a `DOMTagName` property set to `span`', function () {
         assert(new TextOM.Node().DOMTagName === 'span');
     });
+});
 
-    it('should return a span element `toDOMNode()`', function () {
+/**
+ * Unit tests for `TextOM.Node#toDOMNode`.
+ */
+
+describe('Node#toDOMNode()', function () {
+    it('should be a `function`', function () {
+        assert(typeof (new TextOM.Node()).toDOMNode === 'function');
+    });
+
+    it('should return a `<span>` element', function () {
         assert(
             new TextOM.Node().toDOMNode().nodeName.toLowerCase() === 'span'
         );
     });
 });
 
-describe('TextOM.Text#', function () {
-    it('should have a `toDOMNode` method', function () {
+/**
+ * Unit tests for `TextOM.Text#toDOMNode`.
+ */
+
+describe('Text#toDOMNode()', function () {
+    it('should be a `function`', function () {
         assert(typeof (new TextOM.Text()).toDOMNode === 'function');
     });
 
-    it('should return a text node from `toDOMNode()`', function () {
+    it('should return a text node', function () {
         assert(
             new TextOM.Text().toDOMNode().nodeName.toLowerCase() === '#text'
         );
     });
 });
 
-describe('TextOM.RootNode#', function () {
-    it('should have a `toDOMNode` method', function () {
+/**
+ * Unit tests for added properties on `TextOM.RootNode#`.
+ */
+
+describe('RootNode#', function () {
+    it('should have a `DOMTagName` property set to `div`', function () {
+        assert(new TextOM.RootNode().DOMTagName === 'div');
+    });
+});
+
+/**
+ * Unit tests for `TextOM.RootNode#toDOMNode`.
+ */
+
+describe('RootNode#toDOMNode()', function () {
+    it('should be a `function`', function () {
         assert(typeof (new TextOM.RootNode()).toDOMNode === 'function');
     });
 
-    it('should have a `DOMTagName` property set to `span`', function () {
-        assert(new TextOM.RootNode().DOMTagName === 'div');
-    });
-
-    it('should return a span element `toDOMNode()`', function () {
+    it('should return a `<div>` element', function () {
         assert(
             new TextOM.RootNode().toDOMNode().nodeName.toLowerCase() === 'div'
         );
     });
 });
 
-describe('TextOM.ParagraphNode#', function () {
-    it('should have a `toDOMNode` method', function () {
+/**
+ * Unit tests for added properties on `TextOM.ParagraphNode#`.
+ */
+
+describe('ParagraphNode#', function () {
+    it('should have a `DOMTagName` property set to `p`', function () {
+        assert(new TextOM.ParagraphNode().DOMTagName === 'p');
+    });
+});
+
+/**
+ * Unit tests for `TextOM.ParagraphNode#toDOMNode`.
+ */
+
+describe('ParagraphNode#toDOMNode()', function () {
+    it('should be a `function`', function () {
         assert(typeof (new TextOM.ParagraphNode()).toDOMNode === 'function');
     });
 
-    it('should have a `DOMTagName` property set to `span`', function () {
-        assert(new TextOM.ParagraphNode().DOMTagName === 'p');
-    });
-
-    it('should return a span element `toDOMNode()`', function () {
+    it('should return a `<p>` element', function () {
         assert(
             new TextOM.ParagraphNode().toDOMNode()
                 .nodeName.toLowerCase() === 'p'
         );
+    });
+});
+
+/**
+ * Unit tests for live syncing between TextOM and DOM.
+ */
+
+describe('Live workings', function () {
+    it('should create a DOM tree from the TextOM tree', function (done) {
+        retext.parse('Some English words.', function (err, tree) {
+            var $div,
+                sentenceNode,
+                $sentence;
+
+            $div = tree.toDOMNode();
+            sentenceNode = tree.head.head;
+            $sentence = $div.childNodes[0].childNodes[0];
+
+            /**
+             * Validate Paragraph node.
+             */
+
+            assert($div.childNodes[0] === tree.head.toDOMNode());
+
+            /**
+             * Validate Sentence node.
+             */
+
+            assert($sentence === sentenceNode.toDOMNode());
+
+            /**
+             * Validate Text nodes.
+             */
+
+            assert($sentence.childNodes[0] === sentenceNode[0].toDOMNode());
+            assert($sentence.childNodes[1] === sentenceNode[1].toDOMNode());
+            assert($sentence.childNodes[2] === sentenceNode[2].toDOMNode());
+            assert($sentence.childNodes[3] === sentenceNode[3].toDOMNode());
+            assert($sentence.childNodes[4] === sentenceNode[4].toDOMNode());
+            assert($sentence.childNodes[5] === sentenceNode[5].toDOMNode());
+            assert($sentence.childNodes.length === sentenceNode.length);
+
+            /**
+             * Validate HTML.
+             */
+
+            assert($div.outerHTML === '<div>' +
+                    '<p>' +
+                        '<span>' +
+                            '<span>' +
+                                'Some' +
+                            '</span>' +
+                            '<span>' +
+                                ' ' +
+                            '</span>' +
+                            '<span>' +
+                                'English' +
+                            '</span>' +
+                            '<span>' +
+                                ' ' +
+                            '</span>' +
+                            '<span>' +
+                                'words' +
+                            '</span>' +
+                            '<span>' +
+                                '.' +
+                            '</span>' +
+                        '</span>' +
+                    '</p>' +
+                '</div>'
+            );
+
+            done(err);
+        });
+    });
+
+    it('should sync when nodes are removed/inserted', function (done) {
+        retext.parse('Some English words.', function (err, tree) {
+            var $div,
+                sentenceNode,
+                wordNode,
+                whiteSpaceNode,
+                $sentence;
+
+            $div = tree.toDOMNode();
+            sentenceNode = tree.head.head;
+            wordNode = sentenceNode[2];
+            whiteSpaceNode = sentenceNode[3];
+            $sentence = $div.childNodes[0].childNodes[0];
+
+            /**
+             * Remove ``English ''.
+             */
+
+            wordNode.remove();
+            whiteSpaceNode.remove();
+
+            /**
+             * Validate Text nodes.
+             */
+
+            assert($sentence.childNodes[0] === sentenceNode[0].toDOMNode());
+            assert($sentence.childNodes[1] === sentenceNode[1].toDOMNode());
+            assert($sentence.childNodes[2] === sentenceNode[2].toDOMNode());
+            assert($sentence.childNodes[3] === sentenceNode[3].toDOMNode());
+            assert($sentence.childNodes.length === sentenceNode.length);
+
+            /**
+             * Validate HTML.
+             */
+
+            assert(
+                $div.outerHTML === '<div>' +
+                    '<p>' +
+                        '<span>' +
+                            '<span>' +
+                                'Some' +
+                            '</span>' +
+                            '<span>' +
+                                ' ' +
+                            '</span>' +
+                            '<span>' +
+                                'words' +
+                            '</span>' +
+                            '<span>' +
+                                '.' +
+                            '</span>' +
+                        '</span>' +
+                    '</p>' +
+                '</div>'
+            );
+
+            /**
+             * Insert ``English ''.
+             */
+
+            sentenceNode[1].after(whiteSpaceNode);
+            whiteSpaceNode.before(wordNode);
+
+            /**
+             * Validate Text nodes.
+             */
+
+            assert($sentence.childNodes[0] === sentenceNode[0].toDOMNode());
+            assert($sentence.childNodes[1] === sentenceNode[1].toDOMNode());
+            assert($sentence.childNodes[2] === sentenceNode[2].toDOMNode());
+            assert($sentence.childNodes[3] === sentenceNode[3].toDOMNode());
+            assert($sentence.childNodes[4] === sentenceNode[4].toDOMNode());
+            assert($sentence.childNodes[5] === sentenceNode[5].toDOMNode());
+            assert($sentence.childNodes.length === sentenceNode.length);
+
+            /**
+             * Validate HTML.
+             */
+
+            assert($div.outerHTML === '<div>' +
+                    '<p>' +
+                        '<span>' +
+                            '<span>' +
+                                'Some' +
+                            '</span>' +
+                            '<span>' +
+                                ' ' +
+                            '</span>' +
+                            '<span>' +
+                                'English' +
+                            '</span>' +
+                            '<span>' +
+                                ' ' +
+                            '</span>' +
+                            '<span>' +
+                                'words' +
+                            '</span>' +
+                            '<span>' +
+                                '.' +
+                            '</span>' +
+                        '</span>' +
+                    '</p>' +
+                '</div>'
+            );
+
+            done(err);
+        });
+    });
+
+    it('should sync when text values are changed', function (done) {
+        retext.parse('Some English words.', function (err, tree) {
+            var $div;
+
+            $div = tree.toDOMNode();
+
+            /**
+             * Change the terminal marker from a full-stop to a bang.
+             */
+
+            tree.head.head.tail.head.fromString('!');
+
+            /**
+             * Validate text content.
+             */
+
+            assert(tree.head.head.tail.toDOMNode().textContent === '!');
+
+            /**
+             * Validate HTML.
+             */
+
+            assert($div.outerHTML === '<div>' +
+                    '<p>' +
+                        '<span>' +
+                            '<span>' +
+                                'Some' +
+                            '</span>' +
+                            '<span>' +
+                                ' ' +
+                            '</span>' +
+                            '<span>' +
+                                'English' +
+                            '</span>' +
+                            '<span>' +
+                                ' ' +
+                            '</span>' +
+                            '<span>' +
+                                'words' +
+                            '</span>' +
+                            '<span>' +
+                                '!' +
+                            '</span>' +
+                        '</span>' +
+                    '</p>' +
+                '</div>'
+            );
+
+            done(err);
+        });
     });
 });
