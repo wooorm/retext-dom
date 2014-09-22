63784dcb40c183504ace921428a2ea86bdf81995
wooorm <tituswormer@gmail.com>
2014-09-22 14:10:50 +0200

    Refactor API

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 'use strict';
 
-var visit = require('retext-visit');
+var visit;
 
-/* istanbul ignore if: Running in the wrong environment */
+/**
+ * Module dependencies.
+ */
+
+visit = require('retext-visit');
+
+/**
+ * Throw when not running in the browser (or a
+ * simulated browser environment).
+ */
+
+/* istanbul ignore if */
 if (typeof document !== 'object') {
-    throw new Error('Missing document object for retext-dom');
+    throw new Error(
+        'Missing document object for `retext-dom`.'
+    );
 }
+
+/**
+ * Event handler for an inserted TextOM node.
+ *
+ * @param {Node} node - Insertion.
+ */
 
 function oninsertinside(node) {
     node.parent.toDOMNode().insertBefore(node.toDOMNode(),
@@ -13,17 +32,46 @@ function oninsertinside(node) {
     );
 }
 
+/**
+ * Event handler for a removed TextOM node.
+ *
+ * @param {Node} node - Deletion.
+ */
+
 function onremoveinside(node, previousParent) {
     previousParent.toDOMNode().removeChild(node.toDOMNode());
 }
+
+/**
+ * Event handler for a value-change in a TextOM text
+ * node.
+ *
+ * @param {Node} node - Changed node.
+ */
 
 function onchangetextinside(node, newValue) {
     node.toDOMNode().textContent = newValue;
 }
 
+/**
+ * Get the DOM node-equivalent from a TextOM node.
+ *
+ * On initial run, a DOM node is created. If a
+ * `DOMTagName` property exists on the context
+ * a DOM text node is created. Otherwise, an
+ * DOM element is created of type `DOMTagName`.
+ *
+ *
+ * @this {Node}
+ * @return {Node} DOM node.
+ */
+
 function toDOMNode() {
-    var self = this,
-        DOMNode = self.DOMNode;
+    var self,
+        DOMNode;
+
+    self = this;
+    DOMNode = self.DOMNode;
 
     if (!DOMNode) {
         if (!self.DOMTagName) {
@@ -31,33 +79,66 @@ function toDOMNode() {
         } else {
             DOMNode = document.createElement(self.DOMTagName);
         }
+
+        /**
+         * Store DOM node on context.
+         */
+
         self.DOMNode = DOMNode;
+
+        /**
+         * Store context on DOM node.
+         */
+
         DOMNode.TextOMNode = self;
     }
 
     return DOMNode;
 }
 
+/**
+ * Define `plugin`.
+ *
+ * @param {Node} tree - TextOM node.
+ */
+
 function plugin(tree) {
     tree.on('insertinside', oninsertinside);
     tree.on('removeinside', onremoveinside);
     tree.on('changetextinside', onchangetextinside);
 
+    /**
+     * Make sure a parent DOM node, to attach to,
+     * exists.
+     */
+
     tree.toDOMNode();
 
     tree.visit(function (node) {
-        oninsertinside.call(tree, node);
+        oninsertinside(node);
 
         if (node instanceof node.TextOM.Text) {
-            onchangetextinside.call(tree, node, node.toString(), null);
+            onchangetextinside(node, node.toString(), null);
         }
     });
 }
 
+/**
+ * Define `attach`.
+ *
+ * @param {Retext} retext - Instance of Retext.
+ */
+
 function attach(retext) {
-    var TextOM = retext.parser.TextOM;
+    var TextOM;
+
+    /**
+     * Depend on `retext-visit`.
+     */
 
     retext.use(visit);
+
+    TextOM = retext.TextOM;
 
     TextOM.Node.prototype.toDOMNode = toDOMNode;
 
@@ -68,6 +149,14 @@ function attach(retext) {
     TextOM.Text.prototype.DOMTagName = null;
 }
 
+/**
+ * Expose `attach`.
+ */
+
 plugin.attach = attach;
 
-exports = module.exports = plugin;
+/**
+ * Expose `plugin`.
+ */
+
+module.exports = plugin;
